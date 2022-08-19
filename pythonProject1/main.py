109b0e3d0a3a910ea4d68eb7da26a589cf03d254
shaqflair <paapa51@hotmail.com>
2022-08-19 16:32:50 +0100

    First Code
    First Code

---
+++ pythonProject1/main.py
@@ -0,0 +1,22 @@
+print("Alex Adu-Poku")
+print("0-----")
+print(" |||")
+print("*" * 10)
+price = 10
+price = 20
+rating = 4.9
+is_published = True
+print(price)
+name = "John Smith"
+age = 20
+is_new  = True
+name = input("What is your name? ")
+print("Hi " +  name)
+name = input("what is your name? ")
+colour = input("what is your favourite colour? ")
+print( name + " likes " + colour)
+birth_year = input("Birth year: ")
+print(age)
+weight = input("weight")
+
+print(weight)
